8239f60e298e4b72b6920fa43b6af59428e0c12e
LYOUSOFI <hlyousofi@gmail.com>
2025-06-08 14:47:58 +0200

    add config prod

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,14 @@ return [
     |
     */
 
+   
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['https://gmy.garagemaroc.com'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
-    'max_age' => 0,
-
+    'max_age' => 1728000,
     'supports_credentials' => false,
 
 ];
