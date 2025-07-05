bb6b13ea6a84397219bde9b13be7cfa039f1406b
LYOUSOFI <hlyousofi@gmail.com>
2025-07-05 16:26:06 +0200

    update dockerfile and nginx config

--- config/cors.php
+++ config/cors.php
@@ -16,13 +16,13 @@ return [
     */
 
    
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [''],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 1728000,
+    'max_age' => 0,
     'supports_credentials' => false,
 
 ];
